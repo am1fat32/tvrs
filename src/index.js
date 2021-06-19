f15620fb2aea8c91dbcfb4649c031c3a8f5f2380
am1fat32 <igor.kucherenko.dev@gmail.com>
2021-06-20 02:08:36 +0300

    Add operation status displaying

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+const boxen = require('boxen');
 
 const {kebabToPascal} = require('./utils');
 
 const componentName = process.argv[2];
 const rootPath = path.resolve(process.cwd(), componentName);
 
-createComponent();
+createComponent(rootPath);
 
 function createComponent() {
-    if (!fs.existsSync(rootPath)) {
-        createRootFolder();
-        createPackageJSON();
-        createStyles();
-        createTSX();
+    if (fs.existsSync(rootPath)) {
+        console.warn(
+            chalk.red(boxen(`Folder ${chalk.yellow(componentName)} is already exists!`))
+        );
+        return;
     }
+
+    createRootFolder();
+
+    createPackageJSON();
+    createStyles();
+    createTSX();
+
+    console.warn(
+        chalk.green(boxen(`Component ${chalk.yellow(componentName)} was successfully created!`))
+    );
 }
 
 function createRootFolder() {
@@ -22,7 +34,23 @@ function createRootFolder() {
 }
 
 function createPackageJSON() {
-    const content = JSON.stringify(
+    createFile('package.json', createPackageJSONContent())
+}
+
+function createStyles() {
+    createFile(`${componentName}.pcss`, createStylesContent())
+}
+
+function createTSX() {
+    createFile(`${componentName}.tsx`, createTSXContent())
+}
+
+function createFile(fileName, content) {
+    fs.writeFileSync(path.resolve(rootPath, fileName), content)
+}
+
+function createPackageJSONContent() {
+    return JSON.stringify(
         {
             private: true,
             main: componentName,
@@ -31,35 +59,24 @@ function createPackageJSON() {
         null,
         '\t'
     );
-
-    fs.writeFileSync(
-        path.resolve(rootPath, 'package.json'), content
-    )
 }
 
-function createStyles() {
-    fs.writeFileSync(
-        path.resolve(rootPath, `${componentName}.pcss`), ''
-    )
-}
-
-function createTSX() {
-    fs.writeFileSync(
-        path.resolve(rootPath, `${componentName}.tsx`), createTSXContent()
-    )
+function createStylesContent() {
+    return '';
 }
 
 function createTSXContent() {
-    const pascalName = kebabToPascal(componentName);
+    const pascalComponentName = kebabToPascal(componentName);
+    const propsStr = `${pascalComponentName}Props`;
 
     return `
 import React from 'react';
 
 import * as s from './${componentName}.pcss';
 
-export interface ${pascalName}Props {}
+export interface ${propsStr} {}
 
-export function ${pascalName}(props: ${pascalName}Props): JSX.Element {
+export function ${pascalComponentName}(props: ${propsStr}): JSX.Element {
 \treturn (
 \t\t<div>${componentName}</div>
 \t);
