5672eb2ad9d40b78c4d38c25741cf1f17c0dbe75
am1fat32 <igor.kucherenko.dev@gmail.com>
2021-07-22 21:01:18 +0300

    Apply lint rules

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const chalk = require('chalk');
 const boxen = require('boxen');
 
-const {kebabToPascal} = require('./utils');
+const { kebabToPascal } = require('./utils');
 
 const componentName = process.argv[2];
 const rootPath = path.resolve(process.cwd(), componentName);
@@ -11,66 +11,66 @@ const rootPath = path.resolve(process.cwd(), componentName);
 createComponent(rootPath);
 
 function createComponent() {
-    if (fs.existsSync(rootPath)) {
-        console.warn(
-            chalk.red(boxen(`Folder ${chalk.yellow(componentName)} is already exists in this path!`))
-        );
-
-        return;
-    }
-
-    createRootFolder();
-
-    createPackageJSON();
-    createStyles();
-    createTSX();
-
+  if (fs.existsSync(rootPath)) {
     console.warn(
-        chalk.green(boxen(`Component ${chalk.yellow(componentName)} was successfully created!`))
+      chalk.red(boxen(`Folder ${chalk.yellow(componentName)} is already exists in this path!`)),
     );
+
+    return;
+  }
+
+  createRootFolder();
+
+  createPackageJSON();
+  createStyles();
+  createTSX();
+
+  console.warn(
+    chalk.green(boxen(`Component ${chalk.yellow(componentName)} was successfully created!`)),
+  );
 }
 
 function createRootFolder() {
-    fs.mkdirSync(rootPath)
+  fs.mkdirSync(rootPath);
 }
 
 function createPackageJSON() {
-    createFile('package.json', createPackageJSONContent())
+  createFile('package.json', createPackageJSONContent());
 }
 
 function createStyles() {
-    createFile(`${componentName}.pcss`, createStylesContent())
+  createFile(`${componentName}.pcss`, createStylesContent());
 }
 
 function createTSX() {
-    createFile(`${componentName}.tsx`, createTSXContent())
+  createFile(`${componentName}.tsx`, createTSXContent());
 }
 
 function createFile(fileName, content) {
-    fs.writeFileSync(path.resolve(rootPath, fileName), content)
+  fs.writeFileSync(path.resolve(rootPath, fileName), content);
 }
 
 function createPackageJSONContent() {
-    return JSON.stringify(
-        {
-            private: true,
-            main: componentName,
-            typings: `${componentName}.tsx`,
-        },
-        null,
-        2
-    ).concat('\n');
+  return JSON.stringify(
+    {
+      private: true,
+      main: componentName,
+      typings: `${componentName}.tsx`,
+    },
+    null,
+    2,
+  ).concat('\n');
 }
 
 function createStylesContent() {
-    return '';
+  return '';
 }
 
 function createTSXContent() {
-    const pascalComponentName = kebabToPascal(componentName);
-    const propsStr = `${pascalComponentName}Props`;
+  const pascalComponentName = kebabToPascal(componentName);
+  const propsStr = `${pascalComponentName}Props`;
 
-    return `
+  return `
 import React from 'react';
 
 import * as s from './${componentName}.pcss';
