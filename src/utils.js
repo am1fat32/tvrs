675f6fcbcba7498ea1850be99c407b73651ca4cd
am1fat32 <igor.kucherenko.dev@gmail.com>
2021-07-22 21:01:18 +0300

    Apply lint rules

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
-const kebabToPascal = str => str
-    .split('-')
-    .map((it) => it[0].toUpperCase().concat(it.slice(1)))
-    .join('');
+const kebabToPascal = (str) => str
+  .split('-')
+  .map((it) => it[0].toUpperCase().concat(it.slice(1)))
+  .join('');
 
 module.exports = {
-    kebabToPascal,
-}
+  kebabToPascal,
+};
