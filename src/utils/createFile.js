f9e5dbae7e2a88b9db709707023330bba9ed7b62
am1fat32 <igor.kucherenko.dev@gmail.com>
2021-07-23 23:49:24 +0300

    Handle errors and warnings output

--- src/utils/createFile.js
+++ src/utils/createFile.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+const { logToOutput } = require('./logToOutput');
 
 const createFile = (filePath, fileName, template) => {
-  fs.writeFileSync(path.resolve(filePath, fileName), template);
+  const combinedPath = path.resolve(filePath, fileName);
+
+  if (fs.existsSync(combinedPath)) {
+    logToOutput(`${chalk.yellow(fileName)} has been rewritten!`);
+  }
+
+  fs.writeFileSync(combinedPath, template);
 };
 
 module.exports = {
