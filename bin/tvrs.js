55d740a255288667e80fcb1cbd95375169039783
am1fat32 <igor.kucherenko.dev@gmail.com>
2023-07-15 19:36:40 +0400

    Remove redundant options

--- bin/tvrs.js
+++ bin/tvrs.js
@@ -11,16 +11,7 @@ program
   .description('Creates an entity in cwd')
   .argument('<name>', 'entity name in kebab case')
   .argument('[type]', 'entity type: fc | cc', 'fc')
-  .option('-f, --flat', 'create entity without folder')
-  .option('-sp, --skip-package', 'skip package.json')
   .action((entityName, type) => {
-    const { flat, skipPackage } = program.opts();
-
-    createEntity(
-      entityName,
-      type,
-      flat,
-      skipPackage,
-    );
+    createEntity(entityName, type);
   })
   .parse();
