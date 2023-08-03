7480ddbed3675a26a68b8139dfc70fdaaddfefec
ikucherenko <ikucherenko@tradingview.com>
2023-08-03 20:20:07 +0400

    Refactor code

--- src/type-entity.js
+++ src/type-entity.js
@@ -2,3 +2,11 @@ export const TypeEntity = {
   FunctionalComponent: 'fc',
   ClassComponent: 'cc',
 };
+
+export function getPossibleTypeEntities() {
+  return Object.values(TypeEntity);
+}
+
+export function getDefaultTypeEntity() {
+  return TypeEntity.FunctionalComponent;
+}
