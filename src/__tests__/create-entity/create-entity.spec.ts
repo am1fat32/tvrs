07d942c11b7e05d9e2035b603791145de64f7d8a
am1fat32 <fat32dev@gmail.com>
2024-11-13 00:18:28 +0300

    Refactor tests

--- src/__tests__/create-entity/create-entity.spec.ts
+++ src/__tests__/create-entity/create-entity.spec.ts
@@ -6,86 +6,75 @@ import { createEntity } from '../../index';
 jest.createMockFromModule('chalk');
 jest.createMockFromModule('boxen');
 
-const FUNCTIONAL_COMPONENT_NAME = 'test-fc';
-const CLASS_COMPONENT_NAME = 'test-cc';
-
 describe('createEntity', () => {
-  it(`should create "${FUNCTIONAL_COMPONENT_NAME}" functional component`, () => {
-    const tempPath = os.tmpdir();
-    const uniqueTempFolder = path.join(tempPath, `temp-${Date.now()}`);
+  describe('matches the generated files with fixture files', () => {
+    test('passes for generated functional component', () => {
+      const FUNCTIONAL_COMPONENT_NAME = 'test-fc';
 
-    fs.mkdirSync(uniqueTempFolder);
-
-    const targetPath = path.join(uniqueTempFolder, FUNCTIONAL_COMPONENT_NAME);
-
-    createEntity(FUNCTIONAL_COMPONENT_NAME, 'fc', targetPath);
-
-    const createdEntityPath = path.join(
-      uniqueTempFolder,
-      FUNCTIONAL_COMPONENT_NAME,
-    );
-    const fixturesPath = path.join(
-      __dirname,
-      '..',
-      '..',
-      '__fixtures__',
-      FUNCTIONAL_COMPONENT_NAME,
-    );
-
-    const createdFiles = fs.readdirSync(createdEntityPath);
-    const fixturesFiles = fs.readdirSync(fixturesPath);
-
-    expect(createdFiles).toHaveLength(fixturesFiles.length);
-
-    createdFiles.forEach((file) => {
-      const createdFileContent = fs.readFileSync(
-        path.join(createdEntityPath, file),
-        'utf8',
-      );
-      const fixtureFileContent = fs.readFileSync(
-        path.join(fixturesPath, file),
-        'utf8',
+      const uniqueTempDirPath = fs.mkdtempSync(
+        path.join(os.tmpdir(), 'tvrs-test'),
       );
 
-      expect(createdFileContent).toEqual(fixtureFileContent);
+      const entityPath = path.join(
+        uniqueTempDirPath,
+        FUNCTIONAL_COMPONENT_NAME,
+      );
+
+      createEntity(FUNCTIONAL_COMPONENT_NAME, 'fc', entityPath);
+
+      const createdFiles = fs.readdirSync(entityPath);
+      const createdFilesContentList = createdFiles.map((it) =>
+        readFromFile(path.join(entityPath, it)),
+      );
+
+      const fixturesPath = getFixturesPath(FUNCTIONAL_COMPONENT_NAME);
+      const fixturesFiles = fs.readdirSync(fixturesPath);
+      const fixturesFilesContentList = fixturesFiles.map((it) =>
+        readFromFile(path.join(fixturesPath, it)),
+      );
+
+      expect(createdFiles).toHaveLength(fixturesFiles.length);
+      expect(createdFiles).toEqual(expect.arrayContaining(fixturesFiles));
+      expect(createdFilesContentList).toEqual(
+        expect.arrayContaining(fixturesFilesContentList),
+      );
     });
-  });
 
-  it(`should create "${CLASS_COMPONENT_NAME}" class component`, () => {
-    const tempPath = os.tmpdir();
-    const uniqueTempFolder = path.join(tempPath, `temp-${Date.now()}`);
+    test('passes for generated class component', () => {
+      const CLASS_COMPONENT_NAME = 'test-cc';
 
-    fs.mkdirSync(uniqueTempFolder);
-
-    const targetPath = path.join(uniqueTempFolder, CLASS_COMPONENT_NAME);
-
-    createEntity(CLASS_COMPONENT_NAME, 'cc', targetPath);
-
-    const createdEntityPath = path.join(uniqueTempFolder, CLASS_COMPONENT_NAME);
-    const fixturesPath = path.join(
-      __dirname,
-      '..',
-      '..',
-      '__fixtures__',
-      CLASS_COMPONENT_NAME,
-    );
-
-    const createdFiles = fs.readdirSync(createdEntityPath);
-    const fixturesFiles = fs.readdirSync(fixturesPath);
-
-    expect(createdFiles).toHaveLength(fixturesFiles.length);
-
-    createdFiles.forEach((file) => {
-      const createdFileContent = fs.readFileSync(
-        path.join(createdEntityPath, file),
-        'utf8',
-      );
-      const fixtureFileContent = fs.readFileSync(
-        path.join(fixturesPath, file),
-        'utf8',
+      const uniqueTempDirPath = fs.mkdtempSync(
+        path.join(os.tmpdir(), 'tvrs-test'),
       );
 
-      expect(createdFileContent).toEqual(fixtureFileContent);
+      const entityPath = path.join(uniqueTempDirPath, CLASS_COMPONENT_NAME);
+
+      createEntity(CLASS_COMPONENT_NAME, 'cc', entityPath);
+
+      const createdFiles = fs.readdirSync(entityPath);
+      const createdFilesContentList = createdFiles.map((it) =>
+        readFromFile(path.join(entityPath, it)),
+      );
+
+      const fixturesPath = getFixturesPath(CLASS_COMPONENT_NAME);
+      const fixturesFiles = fs.readdirSync(fixturesPath);
+      const fixturesFilesContentList = fixturesFiles.map((it) =>
+        readFromFile(path.join(fixturesPath, it)),
+      );
+
+      expect(createdFiles).toHaveLength(fixturesFiles.length);
+      expect(createdFiles).toEqual(expect.arrayContaining(fixturesFiles));
+      expect(createdFilesContentList).toEqual(
+        expect.arrayContaining(fixturesFilesContentList),
+      );
     });
   });
 });
+
+function getFixturesPath(fixtureDirName: string): string {
+  return path.join(__dirname, '..', '..', '__fixtures__', fixtureDirName);
+}
+
+function readFromFile(path: string): string {
+  return fs.readFileSync(path, 'utf8');
+}
